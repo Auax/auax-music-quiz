777e2039abb8ff6b2e11995158591f718f2ba631
Ibai Farina <ibaifo8@gmail.com>
2022-06-06 15:10:06 +0200

    footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -12,9 +12,8 @@ const Footer = () => {
         <FooterObj className="bg-base100 p-4 text-white relative z-40">
             <div className="grid sm:grid-cols-2 grid-cols-1 w-full mt-0 sm:mt-3">
                 <span
-                    className="text-sm text-center sm:text-left text-neutral-100">Copyright © 2022 - Ibai Farina</span>
-                <hr className="visible sm:hidden my-1 opacity-20 w-full mx-auto"/>
-                <ul className="grid grid-cols-2 gap-3 mx-auto sm:ml-auto sm:mr-0 text-xs text-neutral-400 sm:text-neutral-100">
+                    className="text-sm text-center sm:text-left text-neutral-100 mt-1">Copyright © 2022 - Ibai Farina</span>
+                <ul className="grid grid-cols-2 gap-3 mx-auto sm:ml-auto sm:mr-0 text-xs mt-2 sm:mt-1 text-neutral-400 sm:text-neutral-100">
                     <li className="text-center">
                         <Link to="/privacy-policy" className="hover:underline">Privacy Policy</Link>
                     </li>
